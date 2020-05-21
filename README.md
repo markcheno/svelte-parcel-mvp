91399dd520ec8bbab5adfe5a6e0d487035963792
Mark Chenoweth <mark@cheno.net>
2020-05-20 21:20:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Project Title
+
+Super simple Svelte starter template with Parcel and MVP.css
+
+## Getting Started
+
+```
+npx degit github.com/markcheno/svelte-parcel-mvp my-mvp
+cd my-mvp
+npm install
+npm run dev
+```
+
+## Built With
+
+- [Svelte](https://svelte.dev/) - Cybernetically enhanced web apps
+- [Parcel](https://parceljs.org/) - Blazing fast, zero configuration web application bundler
+- [MVP.css](https://andybrewer.github.io/mvp/) - A minimalist stylesheet for HTML elements
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+- (https://github.com/shff/parcel-svelte-template)
---
+++ app/main.svelte
@@ -0,0 +1,59 @@
+<script>
+  const name = "world";
+</script>
+
+<main>
+  <header>
+    <nav>
+      <a href="/"
+        ><img
+          alt="Logo"
+          src="https://via.placeholder.com/200x70?text=Logo"
+          height="70"
+      /></a>
+      <ul>
+        <li>Menu Item 1</li>
+        <li><a href="#">Menu Item 2</a></li>
+        <li>
+          <a href="#">Dropdown Menu Item</a>
+          <ul>
+            <li><a href="#">Sublink with a long name</a></li>
+            <li><a href="#">Short sublink</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
+    <section>
+    <header>
+      <h2>Section Heading - Hello {name}!</h2>
+      <p>Section Subheading</p>
+    </header>
+    <aside>
+      <h3>Card heading</h3>
+      <p>Card content*</p>
+      <p><small>*with small content</small></p>
+    </aside>
+    <aside>
+      <h3>Card heading</h3>
+      <p>Card content <sup>with notification</sup></p>
+    </aside>
+    <aside>
+      <h3>Card heading</h3>
+      <p>Card content</p>
+    </aside>
+  </section>
+  <div>
+    <details>
+      <summary>Expanable title</summary>
+      <p>Revealed content</p>
+    </details>
+    <details>
+      <summary>Another expanable title</summary>
+      <p>More revealed content</p>
+    </details>
+    <br />
+    <footer>
+        <small>Footer</small>
+    </footer>
+    </main>
